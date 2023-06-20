1434328b4d0384f39ad8d1ac69c11612ced8f719
Alinapikalova <alina.pikalova@gmail.com>
2023-06-20 13:59:42 +0200

    add coge for 01-color-switcher

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,35 @@
+import '../css/common.css';
 
+function getRandomHexColor() {
+  return color =`#${Math.floor(Math.random() * 16777215)
+    .toString(16)
+      .padStart(6, 0)}`;
+    console.log(color);
+}
+
+
+const refs = {
+  body: document.body,
+  btnStart: document.querySelector('button[data-start]'),
+  btnStop: document.querySelector('button[data-stop]'),
+};
+
+const INTERVAL_DELAY = 1000;
+let intervalId = null;
+
+refs.btnStart.addEventListener('click', changeColor);
+refs.btnStop.addEventListener('click', onBtnStop);
+
+function changeColor() {
+    intervalId = setInterval(() => {
+        color = getRandomHexColor();
+        refs.body.style.backgroundColor =
+          color;
+    }, INTERVAL_DELAY);
+    refs.btnStart.disabled = true;
+};
+
+function onBtnStop() {
+  clearInterval(intervalId);
+  refs.btnStart.disabled = false;
+}
