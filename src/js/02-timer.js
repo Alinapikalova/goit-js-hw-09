b71d15a322f005a8e46e3b3038bc2991479c7d51
Alinapikalova <alina.pikalova@gmail.com>
2023-06-25 16:34:03 +0200

    update 02-timer.js

--- src/js/02-timer.js
+++ src/js/02-timer.js
@@ -1,17 +1,42 @@
 import '../css/common.css';
 
 import flatpickr from 'flatpickr';
+import 'flatpickr/dist/flatpickr.min.css';
+
+const btnEl = document.querySelector('button[data-start]');
+const hoursEl = document.querySelector('span[data-hours]');
+const daysEl = document.querySelector('span[data-days]');
+const minutesEl = document.querySelector('span[data-minutas]');
+const secondsEl = document.querySelector('span[data-seconds]');
+
+const dateTimePickar = flatpickr('#datetime-picker', {
+  enableTime: true,
+  time_24hr: true,
+  minDate: 'today',
+  defaultDate: new Date(),
+  minuteIncrement: 1,
+  onClose(selectedDates) {
+    let userDate = selectedDates[0].getTime();
+    const dateNow = new Date();
+
+    if (userDate < dateNow.getTime()) {
+      alert('Please choose a date in the future');
+      btnEl.disabled = true;
+    } else {
+      btnEl.disabled = false;
+    }
+  },
+});
 
 const timer = {
-    start() {
-        const startTime = Date.now();
-        setInterval(() => {
-            console.log('qweqwe');
-        }, 1000);
-    },
+  start() {
+    const startTime = Date.now();
+    setInterval(() => {
+      const currentTime = Date.now();
+      console.log(currentTime - startTime);
+    }, 1000);
+  },
 };
 timer.start();
 
-function updateClockface({ hour, min, sec }) {
-    
-}
+function updateClockface({ hour, min, sec }) {}
